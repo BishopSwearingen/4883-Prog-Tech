5246b346e0374c055ab962e964daa087518bc658
BishopSwearingen <111836157+BishopSwearingen@users.noreply.github.com>
2023-12-14 03:26:22 -0600

    Update 11777main.cpp

--- Assignments/A04/11777main.cpp
+++ Assignments/A04/11777main.cpp
@@ -1,33 +1,39 @@
 //Bishop Swearingen
 //4883
 #include <iostream>
+#include <iomanip>
 #include <algorithm>
 using namespace std;
 
-char determineGrade(float total) {
+char calculateGrade(int total) {
     if (total >= 90) return 'A';
-    else if (total >= 80) return 'B';
-    else if (total >= 70) return 'C';
-    else if (total >= 60) return 'D';
-    else return 'F';
+    if (total >= 80) return 'B';
+    if (total >= 70) return 'C';
+    if (total >= 60) return 'D';
+    return 'F';
 }
 
 int main() {
     int T;
     cin >> T;
-    for (int t = 1; t <= T; t++) {
-        int term1, term2, finalExam, attendance, test1, test2, test3;
-        cin >> term1 >> term2 >> finalExam >> attendance >> test1 >> test2 >> test3;
-        
-        // Calculate the average of the best two tests
-        int tests[] = {test1, test2, test3};
-        sort(tests, tests + 3);
-        float classTestAvg = (tests[1] + tests[2]) / 2.0;
 
-        // Calculate total percentage
-        float total = term1 * 0.20 + term2 * 0.20 + finalExam * 0.30 + attendance * 0.10 + classTestAvg * 0.20;
+    for (int i = 1; i <= T; i++) {
+        int term1, term2, final, attendance, test1, test2, test3;
+        cin >> term1 >> term2 >> final >> attendance >> test1 >> test2 >> test3;
 
-        cout << "Case " << t << ": " << determineGrade(total) << endl;
+        // Calculating the average of best two class tests
+        int minTest = min({test1, test2, test3});
+        int classTestAvg = (test1 + test2 + test3 - minTest) / 2;
+
+        // Calculating the total score
+        int total = term1 + term2 + final + attendance + classTestAvg;
+
+        // Calculating the grade
+        char grade = calculateGrade(total);
+
+        cout << "Case " << i << ": " << grade << endl;
     }
+
     return 0;
 }
+
